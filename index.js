7857e24776e4f922f020229556f9b4aa2d10b15d
HenderBerigo <henderson.berigo@gmail.com>
2025-07-14 09:19:51 -0300

    add lógica if-else do desafio

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("Desafio de Lógica - DIO");
+console.log("Desafio Classificador de nível de Herói");
+
+
+function classifiarHeroi(nome, xp){
+let nivel = "";
+
+  if (xp <= 1000) {
+    nivel = "Ferro";
+  } else if (xp <= 2000) {
+    nivel = "Bronze";
+  } else if (xp <= 5000) {
+    nivel = "Prata";
+  } else if (xp <= 7000) {
+    nivel = "Ouro";
+  } else if (xp <= 8000) {
+    nivel = "Platina";
+  } else if (xp <= 9000) {
+    nivel = "Ascendente";
+  } else if (xp <= 10000) {
+    nivel = "Imortal";
+  } else {
+    nivel = "Radiante";
+  }
+  console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
+
+classifiarHeroi("Mago", 3500);
